1637354d38d66ef111fed79e125664a7885a32b5
Arif Mulpratama <arifdata7@gmail.com>
2025-01-10 06:24:48 +0700

    learning animation flow (all)

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,27 +1,25 @@
-import {Circle, makeScene2D} from '@motion-canvas/2d';
-import {all, createRef, ThreadGenerator} from '@motion-canvas/core';
+import { makeScene2D, Rect } from "@motion-canvas/2d";
+import { all, waitFor, makeRef, range } from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
-  const myCircle = createRef<Circle>();
+  const rects: Rect[] = [];
 
   view.add(
-    <Circle
-      ref={myCircle}
-      width={100}
-      height={100}
-    />
+    range(5).map(i => (
+      <Rect
+        ref={makeRef(rects, i)}
+        width={100}
+        height={100}
+        x={-250 + 125 * i}
+        fill={"#ff0000"}
+        radius={10}
+      />
+    )),
   );
 
-  yield* flicker(myCircle());
+  yield* waitFor(1);
 
-
+  yield* all(
+    ...rects.map(rect => rect.position.y(100, 1).to(-100, 2).to(0, 1)),
+  );
 });
-
-function* flicker(c: Circle): ThreadGenerator {
-  c.fill('red');
-  yield;
-  c.fill('blue');
-  yield;
-  c.fill('green');
-  yield;
-}
