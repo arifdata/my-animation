163b050bd54bf7158576fca98314bcad9a5660b7
Arif Mulpratama <arifdata7@gmail.com>
2025-01-10 09:31:00 +0700

    self experiment: loop and stroke

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,25 +1,34 @@
 import { makeScene2D, Rect } from "@motion-canvas/2d";
-import { all, waitFor, makeRef, range } from "@motion-canvas/core";
+import { all, waitFor, makeRef, range, loop, linear } from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
+  view.fill("#FFFFFF");
   const rects: Rect[] = [];
 
   view.add(
     range(5).map(i => (
       <Rect
         ref={makeRef(rects, i)}
-        width={100}
-        height={100}
+        width={10}
+        height={10}
         x={-250 + 125 * i}
-        fill={"#ff0000"}
-        radius={10}
+        stroke={"#FF0000"}
+        lineWidth={1}
+        // fill={"#ff0000"}
+        // radius={10}
       />
     )),
   );
 
   yield* waitFor(1);
 
+  // yield loop(
+    // () => view.fill("#ff0000", 0.5, linear).to("#00ff00", 0.5, linear),
+  // );
+
   yield* all(
-    ...rects.map(rect => rect.position.y(100, 1).to(-100, 2).to(0, 1)),
+    // ...rects.map(rect => rect.position.y(200, 10)),
+    ...rects.map(rect => rect.scale(10, 0.5)),
+    // ...rects.map(rect => rect.position.y(200, 2)),
   );
 });
