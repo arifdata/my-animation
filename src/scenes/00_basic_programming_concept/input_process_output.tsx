5591cd5efc8da156ac61a8d5994d5133ea1737f1
Arif Mulpratama <arifdata7@gmail.com>
2025-01-13 10:53:37 +0700

    berpusing-pusing ria dengan text

--- src/scenes/00_basic_programming_concept/input_process_output.tsx
+++ src/scenes/00_basic_programming_concept/input_process_output.tsx
@@ -1,6 +1,7 @@
 import {makeScene2D, Camera, Circle, Rect, Layout, Txt} from '@motion-canvas/2d';
-import {all, chain, createRef, waitFor } from '@motion-canvas/core';
-import { appear, disappear } from '../../utils/anims';
+import {all, chain, createRef, easeInOutCubic, waitFor } from '@motion-canvas/core';
+import { appear, disappear, textDisappear } from '../../utils/anims';
+import { ATxt } from '../../utils/nodes/ATxt';
 
 export default makeScene2D(function* (view) {
   const cam = createRef<Camera>();
@@ -8,10 +9,11 @@ export default makeScene2D(function* (view) {
   const r = createRef<Rect>();
   const r1 = createRef<Rect>();
   const r2 = createRef<Rect>();
+  const ww = createRef<Txt>();
 
   var v = view;
 
-  v.fill('#f5f3f4');
+  v.fill('#0000ff');
 
   v.add(
     <>
@@ -21,8 +23,7 @@ export default makeScene2D(function* (view) {
 
           <Rect ref={r} size={300} fill={'#001219'} />
           <Rect layout ref={r1} size={300} fill={'#001219'} alignItems={'center'} direction={'column'}>
-            <Circle size={100} fill={'#ff0000'} />
-            <Txt fill={'#ff0000'} text={"Text coba"} fontSize={64}/>
+            <ATxt ref={ww}>Coba Custom Text</ATxt>
           </Rect>
 
           <Rect ref={r2} size={500} fill={'#001219'} />
@@ -53,8 +54,6 @@ export default makeScene2D(function* (view) {
 
   yield* cam().reset(1);
 
-  yield* cam().zoom(0.5, 1);
-
   yield* chain(
     // r().size(0, 0.3),
     // r1().size(0, 0.3),
@@ -63,6 +62,15 @@ export default makeScene2D(function* (view) {
     disappear(r1()),
     disappear(r2()),
   );
+  yield* waitFor(1);
+
+  yield* all(
+    ww().scale(5, 1, easeInOutCubic),
+    ww().rotation(-90, 1),
+  );
+  yield* ww().text("Coba ganti teks", 1);
+
+  yield* textDisappear(ww());
   yield* waitFor(1);
 
 });
--- src/utils/anims.ts
+++ src/utils/anims.ts
@@ -1,11 +1,11 @@
 import { ThreadGenerator, all, easeInOutCubic } from "@motion-canvas/core";
-import { Shape } from "@motion-canvas/2d";
+import { Shape, Txt } from "@motion-canvas/2d";
 
 export function* appear(object: Shape, duration = 0.5): ThreadGenerator {
     let objSize = object.size();
 
     yield* all(
-        object.size(0).size(objSize, duration, easeInOutCubic),
+      object.size(0).size(objSize, duration, easeInOutCubic),
     );
 }
 
@@ -13,6 +13,16 @@ export function* disappear(object: Shape, duration = 0.5): ThreadGenerator {
     let objSize = object.size();
 
     yield* all(
-        object.size(objSize).size(0, duration, easeInOutCubic),
+      object.size(objSize).size(0, duration, easeInOutCubic),
+      object.rotation(90, duration),
     );
+
+}
+
+export function* textDisappear(object: Txt, duration = 0.5): ThreadGenerator {
+
+    yield* all(
+      object.scale(0, duration, easeInOutCubic),
+    );
+
 }
