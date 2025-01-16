c89ec3b0946d70c0b55b5bd2d587ac941265d8c4
Arif Mulpratama <arifdata7@gmail.com>
2025-01-16 11:45:35 +0700

    experimenting with Signal, its awesome!

--- src/scenes/00_basic_programming_concept/input_process_output.tsx
+++ src/scenes/00_basic_programming_concept/input_process_output.tsx
@@ -1,90 +1,64 @@
-import {makeScene2D, Camera, Circle, Rect, Layout, Txt} from '@motion-canvas/2d';
-import {all, chain, createRef, easeInOutCirc, easeInOutCubic, easeInOutExpo, linear, waitFor } from '@motion-canvas/core';
-import { appear, disappear, textDisappear } from '../../utils/anims';
+import {makeScene2D, Circle, Txt} from '@motion-canvas/2d';
+import {all, createRef, createSignal, loopFor, useLogger, useRandom, waitFor } from '@motion-canvas/core';
 import { ATxt } from '../../utils/nodes/ATxt';
-import { black, gray, darkBlue, lightBlue, blue, orange, maroon, yellow, reddish, thickOrange } from '../../color-palettes/ten-colorful';
+import { gray, black, maroon } from '../../color-palettes/ten-colorful';
 
 export default makeScene2D(function* (view) {
-  const cam = createRef<Camera>();
-  const r1 = createRef<Rect>();
-  const ww = createRef<Txt>();
-  const cc = createRef<Circle>();
+  const myCircle = createRef<Circle>();
+  const rnd = useRandom();
+  const txt = createRef<Txt>();
+  // const xVal = createSignal(0);
+  // const yVal = createSignal(() => linearFunction(xVal()));
+  //
+  const sizeSignal = createSignal(0);
+  const xTextVal = createSignal(0);
+  const yTextVal = createSignal(0);
+  // const log = useLogger();
+  view.fill(black);
 
-  var v = view;
-
-  v.fill(black);
-
-  v.add(
+  view.add(
     <>
-      <Camera ref={cam}>
+    <Circle
+      ref={myCircle}
+      // try changing these properties:
+      // x={() => xVal()}
+      // y={() => yVal()}
+      // width={20}
+      // height={20}
+      size={() => sizeSignal()}
+      fill={maroon}
+    />
 
+      <ATxt
+        ref={txt}
+        text={() => `Size: ${sizeSignal().toFixed()}`}
+        x={() => xTextVal()}
+        y={() => yTextVal()}
+      />
 
-          <Rect layout paddingTop={20} paddingLeft={20} ref={r1} width={300} height={300} fill={thickOrange} smoothCorners={true} radius={30} >
-            <ATxt ref={ww} scale={0} fill={gray}>Input</ATxt>
-          </Rect>
-
-          <Circle ref={cc}
-          size={900}
-          // fill={'#ffff00'}
-          
-          />
-
-
-
-      </Camera>
-
-    </>
+      </>
   );
 
-  yield* all(
-    appear(r1()),
+  // yield* txt().filters.blur(2);
+
+  // yield* all(
+    // xVal((xVal() * 10) + 1, 1),
+  // );
+  yield* loopFor(
+    30,
+    // () => sizeSignal(rnd.nextInt(300, 1000), 1)
+    () => all(
+      sizeSignal(rnd.nextInt(300, 1000), 1),
+      xTextVal(rnd.nextInt(-800, 800), 1),
+      yTextVal(rnd.nextInt(-500, 500), 1),
+    )
   );
-
-
-
-  yield* r1().size(500, 1);
-
-  // yield* ww().position(r1().topLeft().add([70, 30]), 1, easeInOutCirc);
-
-  yield* ww().scale(1, 1);
-  yield* r1().ripple();
-
-  yield* ww().text("Menyengsarakan", 1);
-
-  yield* all(
-    r1().rotation(360, 2, easeInOutCubic),
-    r1().position.x(300, 2, easeInOutCubic),
-  );
-
-  // yield* r1().rotation(360, 2, easeInOutCubic);
-
-  // yield* r().ripple();
-  yield* all(
-    cam().zoom(2, 1),
-  );
-
-  yield* cam().reset(1);
-
-  yield* r1().scale(0.1, 1);
-
-  yield* all(
-    // r().size(0, 0.3),
-    // r1().size(0, 0.3),
-    // r2().size(0, 0.3),
-    disappear(r1()),
-    textDisappear(ww()),
-  );
-  yield* waitFor(1);
-
-  yield* all(
-    ww().scale(5, 1, easeInOutCubic),
-  );
-  yield* ww().text("Coba ganti 😎 teks", 1);
-
-  yield* cam().followCurve(cc(), 5, linear);
-
-  yield* textDisappear(ww());
+  // yield* xVal(-10, 5).to(-50, 5).to(-1000, 5);
+  // yield* sizeSignal(rnd.nextInt(0, 100), 1).to(rnd.nextInt(0, 100), 1).to(rnd.nextInt(0, 100), 1);
   yield* waitFor(1);
 
 });
 
+function linearFunction(xValue: number): number {
+  return xValue + 50;
+}
