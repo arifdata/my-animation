cb135feee18013c5a10e76126884edc112f500fb
Arif Mulpratama <arifdata7@gmail.com>
2025-02-23 12:07:41 +0700

    saving progress accumulate function scene

--- src/scenes/excel_episode_00/accumulate_function.tsx
+++ src/scenes/excel_episode_00/accumulate_function.tsx
@@ -1,10 +1,11 @@
-import {makeScene2D, Layout, Img} from '@motion-canvas/2d';
-import {all, createRef, easeInExpo, easeInOutExpo, waitFor, u} from '@motion-canvas/core';
+// RENDER DENGAN RESOLUSI 970 X 1080
+import {makeScene2D, Layout, Img, Rect} from '@motion-canvas/2d';
+import {all, createRef, easeInOutExpo, waitFor} from '@motion-canvas/core';
 import { CellContent, WorkbookObject } from '../../components/CellContents';
 import { LoopElements } from '../../components/LoopElements';
 import { createSignal } from '@motion-canvas/core';
-import { useLogger } from '@motion-canvas/core';
 import gambar from './exce_file.png';
+import { orange } from '../../color-palettes/five-colorful';
 
 export default makeScene2D(function* (view) {
   const loopItems = [{"Paracetamol tablet 500 mg": 30}, {"Ambroxol tablet 30 mg": 20}, {"Paracetamol tablet 500 mg": 30}, {"Metformin tablet 500 mg": 20}, {"Ambroxol tablet 30 mg": 10}, {"Ibuprofen tablet 400 mg": 20}, {"Metformin tablet 500 mg": 30}, {"Amlodipine tablet 5 mg": 80}, {"Ibuprofen tablet 400 mg": 30}, {"Paracetamol tablet 500 mg": 30}];
@@ -12,35 +13,68 @@ export default makeScene2D(function* (view) {
   const image = createRef<Img>();
   const loopElements = createRef<Layout>();
   const workBookObject = createRef<Layout>();
-  const posX = createSignal(0);
+  const customRect = createRef<Rect>();
+  const posX = createSignal(700);
   const posY = createSignal(0);
 
 
   view.fill('#ffffff');
 
   view.add(
-    <WorkbookObject />
+    <WorkbookObject ref={workBookObject}/>
   );
+  workBookObject().x(() => posX());
+  workBookObject().y(() => posY());
+
 
   view.add(
     <Img ref={image} src={gambar} x={() => posX()} y={() => posY()} scale={1} radius={20}/>
   );
-  yield image().scale(0);
+  image().scale(0);
 
   view.add(
     <LoopElements ref={loopElements} elements={loopItems}/>
   );
-  yield loopElements().x(() => posX());
-  yield loopElements().y(() => posY());
-  yield loopElements().scale(0);
+  loopElements().x(() => posX());
+  loopElements().y(() => posY());
+  loopElements().scale(0);
 
-  // yield* all(
-  //   posX(-300, 1, easeInOutExpo),
-  //   posY(-500, 1, easeInOutExpo),
-  // );
+  yield* posX(0, 1, easeInOutExpo);
+
+  yield* waitFor(0.5);
+
+  yield* all(
+    workBookObject().rotation(360, 1, easeInOutExpo),
+    workBookObject().opacity(0, 1),
+
+    image().rotation(360, 1, easeInOutExpo),
+    image().scale(1, 1),
+  );
+
+  yield view.add(
+    <Rect fill={orange} width={100} height={30} ref={customRect} opacity={0.5} radius={10} x={800} y={150}/>
+  );
+
+  yield* customRect().x(-120, 1, easeInOutExpo);
+
+  yield* waitFor(0.5);
+
+  yield* customRect().ripple();
+
+  yield* waitFor(0.5);
+
+  yield* all(
+    customRect().opacity(0, 0.5),
+    image().rotation(0, 1, easeInOutExpo),
+    image().opacity(0, 1),
+
+    loopElements().rotation(360, 1, easeInOutExpo),
+    loopElements().scale(1, 1),
+  );
+
+  yield* waitFor(0.5);
 
 
-  yield* waitFor(1);
 
 
 });
