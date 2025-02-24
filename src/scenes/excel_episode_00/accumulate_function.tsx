67790ab2026ef14610e687c91be060d4b86af898
Arif Mulpratama <arifdata7@gmail.com>
2025-02-24 08:54:23 +0700

    change duration

--- src/scenes/excel_episode_00/accumulate_function.tsx
+++ src/scenes/excel_episode_00/accumulate_function.tsx
@@ -5,7 +5,7 @@ import { CellContent, Object } from '../../components/CellContents';
 import { LoopElements } from '../../components/LoopElements';
 import { createSignal } from '@motion-canvas/core';
 import gambar from './exce_file.png';
-import { reddish, darkBlue, orange, thickOrange, reddish, yellow } from '../../color-palettes/ten-colorful'; 
+import {  gray, maroon, darkBlue, orange, thickOrange, reddish, yellow } from '../../color-palettes/ten-colorful'; 
 import { ATxt } from '../../utils/nodes/ATxt';
 import { black } from '../../color-palettes/five-colorful';
 import sheet2_png_0 from './sheet_2_0.png';
@@ -52,40 +52,40 @@ export default makeScene2D(function* (view) {
   yield* waitFor(0.5);
 
   yield* all(
-    workBookObject().rotation(360, 1, easeInOutExpo),
-    workBookObject().opacity(0, 1),
+    workBookObject().rotation(360, 0.5, easeInOutExpo),
+    workBookObject().opacity(0, 0.5),
 
-    image().rotation(360, 1, easeInOutExpo),
-    image().scale(1, 1),
+    image().rotation(360, 0.5, easeInOutExpo),
+    image().scale(1, 0.5),
   );
 
   yield view.add(
     <Rect fill={orange} width={80} height={25} ref={customRect} opacity={0.5} radius={10} x={800} y={145}/>
   );
 
-  yield* customRect().x(-125, 1, easeInOutExpo);
+  yield* customRect().x(-125, 0.5, easeInOutExpo);
 
   yield* waitFor(0.5);
 
-  yield* customRect().ripple();
+  yield* customRect().ripple(0.5);
 
   yield* waitFor(0.5);
 
   yield* all(
     customRect().opacity(0, 0.5),
-    image().rotation(0, 1, easeInOutExpo),
-    image().opacity(0, 1),
+    image().rotation(0, 0.5, easeInOutExpo),
+    image().opacity(0, 0.5),
 
-    loopElements().rotation(360, 1, easeInOutExpo),
-    loopElements().scale(1, 1),
+    loopElements().rotation(360, 0.5, easeInOutExpo),
+    loopElements().scale(1, 0.5),
   );
 
   yield* waitFor(0.5);
 
   yield* all(
-    posX(-300, 1, easeInOutExpo),
-    posY(500, 1, easeInOutExpo),
-    loopElements().gap(30, 1),
+    posX(-300, 0.5, easeInOutExpo),
+    posY(500, 0.5, easeInOutExpo),
+    loopElements().gap(30, 0.5),
   );
 
   view.add(
@@ -94,23 +94,23 @@ export default makeScene2D(function* (view) {
   dataObject().x(100);
   dataObject().y(-800);
 
-  yield* dataObject().y(-400, 1, easeInOutExpo);
+  yield* dataObject().y(-400, 0.5, easeInOutExpo);
 
   yield* waitFor(0.5);
 
   yield* all(
-    customRect().opacity(0.5, 1),
-    customRect().width(150, 1, easeInOutExpo),
-    customRect().height(50, 1, easeInOutExpo),
-    customRect().fill(reddish, 1),
-    customRect().opacity(0.3, 1),
-    customRect().x(-320, 1, easeInOutExpo),
-    customRect().y(140, 1 ,easeInOutExpo),
+    customRect().opacity(0.5, 0.5),
+    customRect().width(150, 0.5, easeInOutExpo),
+    customRect().height(50, 0.5, easeInOutExpo),
+    customRect().fill(reddish, 0.5),
+    customRect().opacity(0.5, 0.5),
+    customRect().x(-320, 0.5, easeInOutExpo),
+    customRect().y(140, 0.5,easeInOutExpo),
   );
 
   yield* waitFor(0.5);
 
-  yield* customRect().ripple();
+  yield* customRect().ripple(0.5);
 
   cellReferences.push(createRef<Layout>());
   view.add(
@@ -118,14 +118,14 @@ export default makeScene2D(function* (view) {
   );
 
   yield* all(
-    cellReferences[0]().x(0, 1, easeInOutExpo),
-    cellReferences[0]().y(-400, 1, easeInOutExpo),
+    cellReferences[0]().x(0, 0.5, easeInOutExpo),
+    cellReferences[0]().y(-400, 0.5, easeInOutExpo),
     cellReferences[0]().opacity(1, 0.5),
   );
 
-  yield* posY(420, 1, easeInOutExpo);
+  yield* posY(420, 0.5, easeInOutExpo);
 
-  yield* customRect().ripple();
+  yield* customRect().ripple(0.5);
 
   cellReferences.push(createRef<Layout>());
 
@@ -134,12 +134,12 @@ export default makeScene2D(function* (view) {
   );
 
   yield* all(
-    cellReferences[1]().x(150, 1, easeInOutExpo),
-    cellReferences[1]().y(-370, 1, easeInOutExpo),
+    cellReferences[1]().x(150, 0.5, easeInOutExpo),
+    cellReferences[1]().y(-370, 0.5, easeInOutExpo),
     cellReferences[1]().opacity(1, 0.5),
   );
 
-  yield* posY(340, 1, easeInOutExpo);
+  yield* posY(340, 0.5, easeInOutExpo);
 
   cellReferences.push(createRef<Txt>());
 
@@ -149,20 +149,20 @@ export default makeScene2D(function* (view) {
 
   yield* chain(
     all(
-      cellReferences[2]().x(() => cellReferences[0]().x(), 1, easeInOutExpo),
-      cellReferences[2]().y(() => cellReferences[0]().y(), 1, easeInOutExpo),
-      cellReferences[2]().opacity(1, 0).to(0, 1),
+      cellReferences[2]().x(() => cellReferences[0]().x(), 0.5, easeInOutExpo),
+      cellReferences[2]().y(() => cellReferences[0]().y(), 0.5, easeInOutExpo),
+      cellReferences[2]().opacity(1, 0).to(0, 0.5),
     ),
 
     all(
       cellReferences[0]().children()[1].children()[0].text("60", 0),
-      cellReferences[0]().children()[1].ripple(),
+      cellReferences[0]().children()[1].ripple(0.5),
     ),
   );
 
-  yield* posY(260, 1, easeInOutExpo);
+  yield* posY(260, 0.5, easeInOutExpo);
 
-  yield* customRect().ripple();
+  yield* customRect().ripple(0.5);
 
   cellReferences.push(createRef<Layout>());
 
@@ -171,17 +171,17 @@ export default makeScene2D(function* (view) {
   );
 
   yield* all(
-    dataObject().children()[1].width(500, 1, easeInOutExpo),
-    dataObject().children()[1].height(400, 1, easeInOutExpo),
-    dataObject().x(170, 1, easeInOutExpo),
-    dataObject().y(-300, 1, easeInOutExpo),
+    dataObject().children()[1].width(500, 0.5, easeInOutExpo),
+    dataObject().children()[1].height(400, 0.5, easeInOutExpo),
+    dataObject().x(170, 0.5, easeInOutExpo),
+    dataObject().y(-300, 0.5, easeInOutExpo),
 
-    cellReferences[3]().x(300, 1, easeInOutExpo),
-    cellReferences[3]().y(-400, 1, easeInOutExpo),
+    cellReferences[3]().x(300, 0.5, easeInOutExpo),
+    cellReferences[3]().y(-400, 0.5, easeInOutExpo),
     cellReferences[3]().opacity(1, 0.5),
   );
 
-  yield* posY(180, 1, easeInOutExpo);
+  yield* posY(180, 0.5, easeInOutExpo);
 
   cellReferences.push(createRef<Txt>());
 
@@ -191,20 +191,20 @@ export default makeScene2D(function* (view) {
 
   yield* chain(
     all(
-      cellReferences[4]().x(() => cellReferences[1]().x(), 1, easeInOutExpo),
-      cellReferences[4]().y(() => cellReferences[1]().y(), 1, easeInOutExpo),
-      cellReferences[4]().opacity(1, 0).to(0, 1),
+      cellReferences[4]().x(() => cellReferences[1]().x(), 0.5, easeInOutExpo),
+      cellReferences[4]().y(() => cellReferences[1]().y(), 0.5, easeInOutExpo),
+      cellReferences[4]().opacity(1, 0).to(0, 0.5),
     ),
 
     all(
       cellReferences[1]().children()[1].children()[0].text("30", 0),
-      cellReferences[1]().children()[1].ripple(),
+      cellReferences[1]().children()[1].ripple(0.5),
     ),
   );
 
-  yield* posY(100, 1, easeInOutExpo);
+  yield* posY(100, 0.5, easeInOutExpo);
 
-  yield* customRect().ripple();
+  yield* customRect().ripple(0.5);
 
   cellReferences.push(createRef<Layout>());
 
@@ -213,12 +213,12 @@ export default makeScene2D(function* (view) {
   );
 
   yield* all(
-    cellReferences[5]().x(50, 1, easeInOutExpo),
-    cellReferences[5]().y(-250, 1, easeInOutExpo),
-    cellReferences[5]().opacity(1, 0.5),
+    cellReferences[5]().x(50, 0.5, easeInOutExpo),
+    cellReferences[5]().y(-250, 0.5, easeInOutExpo),
+    cellReferences[5]().opacity(1, 0.3),
   );
 
-  yield* posY(20, 1, easeInOutExpo);
+  yield* posY(20, 0.5, easeInOutExpo);
   cellReferences.push(createRef<Txt>());
 
   view.add(
@@ -227,20 +227,20 @@ export default makeScene2D(function* (view) {
 
   yield* chain(
     all(
-      cellReferences[6]().x(() => cellReferences[3]().x(), 1, easeInOutExpo),
-      cellReferences[6]().y(() => cellReferences[3]().y(), 1, easeInOutExpo),
-      cellReferences[6]().opacity(1, 0).to(0, 1),
+      cellReferences[6]().x(() => cellReferences[3]().x(), 0.5, easeInOutExpo),
+      cellReferences[6]().y(() => cellReferences[3]().y(), 0.5, easeInOutExpo),
+      cellReferences[6]().opacity(1, 0).to(0, 0.5),
     ),
 
     all(
       cellReferences[3]().children()[1].children()[0].text("50", 0),
-      cellReferences[3]().children()[1].ripple(),
+      cellReferences[3]().children()[1].ripple(0.5),
     ),
   );
 
-  yield* posY(-60, 1, easeInOutExpo);
+  yield* posY(-60, 0.5, easeInOutExpo);
 
-  yield* customRect().ripple();
+  yield* customRect().ripple(0.5);
 
   cellReferences.push(createRef<Layout>());
 
@@ -249,12 +249,12 @@ export default makeScene2D(function* (view) {
   );
 
   yield* all(
-    cellReferences[7]().x(250, 1, easeInOutExpo),
-    cellReferences[7]().y(-200, 1, easeInOutExpo),
-    cellReferences[7]().opacity(1, 0.5),
+    cellReferences[7]().x(250, 0.5, easeInOutExpo),
+    cellReferences[7]().y(-200, 0.5, easeInOutExpo),
+    cellReferences[7]().opacity(1, 0.3),
   );
 
-  yield* posY(-140, 1, easeInOutExpo);
+  yield* posY(-140, 0.5, easeInOutExpo);
 
   cellReferences.push(createRef<Txt>());
 
@@ -264,18 +264,18 @@ export default makeScene2D(function* (view) {
 
   yield* chain(
     all(
-      cellReferences[8]().x(() => cellReferences[5]().x(), 1, easeInOutExpo),
-      cellReferences[8]().y(() => cellReferences[5]().y(), 1, easeInOutExpo),
-      cellReferences[8]().opacity(1, 0).to(0, 1),
+      cellReferences[8]().x(() => cellReferences[5]().x(), 0.5, easeInOutExpo),
+      cellReferences[8]().y(() => cellReferences[5]().y(), 0.5, easeInOutExpo),
+      cellReferences[8]().opacity(1, 0).to(0, 0.5),
     ),
 
     all(
       cellReferences[5]().children()[1].children()[0].text("50", 0),
-      cellReferences[5]().children()[1].ripple(),
+      cellReferences[5]().children()[1].ripple(0.5),
     ),
   );
 
-  yield* posY(-220, 1, easeInOutExpo);
+  yield* posY(-220, 0.5, easeInOutExpo);
 
   cellReferences.push(createRef<Txt>());
 
@@ -285,14 +285,14 @@ export default makeScene2D(function* (view) {
 
   yield* chain(
     all(
-      cellReferences[9]().x(() => cellReferences[0]().x(), 1, easeInOutExpo),
-      cellReferences[9]().y(() => cellReferences[0]().y(), 1, easeInOutExpo),
-      cellReferences[9]().opacity(1, 0).to(0, 1),
+      cellReferences[9]().x(() => cellReferences[0]().x(), 0.5, easeInOutExpo),
+      cellReferences[9]().y(() => cellReferences[0]().y(), 0.5, easeInOutExpo),
+      cellReferences[9]().opacity(1, 0).to(0, 0.5),
     ),
 
     all(
       cellReferences[0]().children()[1].children()[0].text("90", 0),
-      cellReferences[0]().children()[1].ripple(),
+      cellReferences[0]().children()[1].ripple(0.5),
     ),
   );
 
@@ -314,7 +314,7 @@ export default makeScene2D(function* (view) {
       dataObject().children()[1].width(150, 0.5),
       dataObject().children()[1].height(80, 0.5),
 
-      dataObject().scale(0.7, 1, easeInOutExpo),
+      dataObject().scale(0.7, 0.5, easeInOutExpo),
 
     ),
 
@@ -344,15 +344,15 @@ export default makeScene2D(function* (view) {
 
   yield* all(
     workBookObject().children()[1].add(sheet2_txt()),
-    workBookObject().scale(0, 1),
+    workBookObject().scale(0, 0.5),
     image().x(0, 0),
     image().y(0, 0),
-    image().opacity(1, 1),
-    image().scale(0, 0).to(1, 1),
+    image().opacity(1, 0.5),
+    image().scale(0, 0).to(1, 0.5),
   );
 
   yield* all(
-    image().ripple(),
+    image().ripple(0.5),
     image().src(sheet2_png_0, 0),
   );
 
@@ -361,11 +361,11 @@ export default makeScene2D(function* (view) {
   );
 
   yield* all(
-    dataObject().scale(1, 1),
-    dataObject().children()[1].width(150, 1),
-    dataObject().children()[1].height(150, 1),
-    dataObject().y(-430, 1),
-    dataObject().x(-370, 1),
+    dataObject().scale(1, 0.5),
+    dataObject().children()[1].width(150, 0.5),
+    dataObject().children()[1].height(150, 0.5),
+    dataObject().y(-430, 0.5),
+    dataObject().x(-370, 0.5),
   );
 
   const explainer = createRef<Txt>();
@@ -385,17 +385,17 @@ export default makeScene2D(function* (view) {
 
   yield* chain(
     all(
-      cellReferences[0]().x(0, 1, easeInOutExpo),
-      cellReferences[0]().y(0, 1, easeInOutExpo),
-      cellReferences[0]().scale(0, 0.8),
-      sheet2_txt().ripple(),
+      cellReferences[0]().x(0, 0.5, easeInOutExpo),
+      cellReferences[0]().y(0, 0.5, easeInOutExpo),
+      cellReferences[0]().scale(0, 0.3),
+      sheet2_txt().ripple(0.5),
       explainer().text(`A1 = Paracetamol\nB1 = 90`, 1),
     ),
 
     all(
-      image().ripple(),
+      image().ripple(0.5),
       image().src(sheet2_png_1, 0),
-      explainer().text("", 0.7),
+      explainer().text("", 0.5),
     ),
   );
 
@@ -404,21 +404,21 @@ export default makeScene2D(function* (view) {
     cellReferences[1]().scale(0, 0).to(0.6, 0.5),
     cellReferences[1]().x(-390, 0),
     cellReferences[1]().y(-410, 0),
-    explainer().text(`f"A{counter}" = drug_name\nf"B{counter}" = amount`, 1),
+    explainer().text(`f"A{counter}" = drug_name\nf"B{counter}" = amount`, 0.5),
   );
 
   yield* chain(
     all(
-      cellReferences[1]().x(0, 1, easeInOutExpo),
-      cellReferences[1]().y(0, 1, easeInOutExpo),
-      cellReferences[1]().scale(0, 0.8),
-      sheet2_txt().ripple(),
-      explainer().text(`A1 = Ambroxol\nB1 = 30`, 1),
+      cellReferences[1]().x(0, 0.5, easeInOutExpo),
+      cellReferences[1]().y(0, 0.5, easeInOutExpo),
+      cellReferences[1]().scale(0, 0.3),
+      sheet2_txt().ripple(0.5),
+      explainer().text(`A1 = Ambroxol\nB1 = 30`, 0.5),
     ),
     all(
-      image().ripple(),
+      image().ripple(0.5),
       image().src(sheet2_png_2, 0),
-      explainer().text("", 0.7),
+      explainer().text("", 0.3),
     ),
   );
 
