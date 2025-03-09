4cf31cfd9f62bdae6f93347301e22fb610c1a22a
Arif Mulpratama <arifdata7@gmail.com>
2025-03-09 22:47:22 +0700

    spline path

--- src/scenes/00_basic_programming_concept/what_is_programming.tsx
+++ src/scenes/00_basic_programming_concept/what_is_programming.tsx
@@ -1,5 +1,5 @@
-import { createRef, all, easeInQuad, easeInOutBounce, linear, easeInOutCubic, easeOutBounce, easeOutCirc, easeOutElastic, easeInBounce, waitFor, sequence } from "@motion-canvas/core";
-import { makeScene2D, Circle, Rect, Camera, Icon, Txt } from "@motion-canvas/2d";
+import { createRef, all, easeInQuad, easeInOutBounce, linear, easeInOutCubic, easeOutBounce, easeOutCirc, easeOutElastic, easeInBounce, waitFor, sequence, createSignal } from "@motion-canvas/core";
+import { makeScene2D, Circle, Rect, Camera, Icon, Txt, Spline } from "@motion-canvas/2d";
 import { ATxt } from "../../utils/nodes/ATxt";
 import { appear, disappear } from "../slamaDev/utilities";
 import {white, black, blue, red, orange} from "../../color-palettes/five-colorful";
@@ -33,7 +33,7 @@ export default makeScene2D(function* (view) {
   const process = [];
   process.push(createRef<Rect>());
   view.add(
-    <Rect ref={process[0]} fill={blue} width={300} height={200} radius={10} zIndex={1}>
+    <Rect ref={process[0]} fill={blue} width={300} height={200} radius={10} zIndex={2}>
       <ATxt text={"Process"} fill={black} scale={2}/>
     </Rect>
   );
@@ -53,8 +53,48 @@ export default makeScene2D(function* (view) {
     input[0]().x(0, 0.5, easeInOutCubic),
     process[0]().ripple(),
     output[0]().x(400, 0.5, easeInOutCubic),
-    disappear(output[0]()),
   );
+
+  yield* waitFor(0.5);
+
+  process.push(createRef<Rect>());
+  view.add(
+    <Rect ref={process[1]} fill={red} width={300} height={200} radius={10} zIndex={1}>
+      <ATxt text={"Process"} fill={black} scale={2}/>
+    </Rect>
+  );
+  process.push(createRef<Rect>());
+  view.add(
+    <Rect ref={process[2]} fill={orange} width={300} height={200} radius={10} zIndex={1}>
+      <ATxt text={"Process"} fill={black} scale={2}/>
+    </Rect>
+  );
+
+  const spline1 = createRef<Spline>();
+  const progress1 = createSignal(0);
+  view.add(
+    <Spline
+      ref={spline1}
+      // lineWidth={1}
+      // stroke={black}
+      points={[[400, 0], [200, -800], [-400, -500]]}
+      smoothness={1}
+    />
+  );
+
+  yield* output[0]().position(() => spline1().getPointAtPercentage(progress1()).position);
+
+  yield* sequence(0.5,
+    all(
+
+    process[1]().y(-500, 0.5, easeInOutCubic),
+    process[2]().y(500, 0.5, easeInOutCubic),
+    ),
+    progress1(1, 1, easeInOutCubic),
+    // output[0]().x(-400, 0.5, easeInOutCubic),
+    // output[0]().y(-500, 0.5, easeInOutCubic),
+  );
+
 
   yield* waitFor(1);
 });
