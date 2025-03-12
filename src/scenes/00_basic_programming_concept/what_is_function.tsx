276268a1da33132a9b4f52c8c1ba186568c62b4a
Arif Mulpratama <arifdata7@gmail.com>
2025-03-12 12:02:38 +0700

    sudah selesaikan akhir animasi, next add top to down instruction animation

--- src/scenes/00_basic_programming_concept/what_is_function.tsx
+++ src/scenes/00_basic_programming_concept/what_is_function.tsx
@@ -2,7 +2,7 @@ import {makeScene2D, Code, Layout, word} from '@motion-canvas/2d';
 import {all, createRef, DEFAULT, easeInOutCubic, sequence, waitFor} from '@motion-canvas/core';
 import { white, black, blue, red, orange } from '../../color-palettes/five-colorful';
 import { Object } from '../../components/CellContents';
-import { appear } from '../slamaDev/utilities';
+import { appear, disappear } from '../slamaDev/utilities';
 
 export default makeScene2D(function* (view) {
   view.fill(white);
@@ -59,13 +59,40 @@ export default makeScene2D(function* (view) {
   yield* code().code('def greet():\n    pass', 1);
   yield* code().code('def greet():\n    print("Hello world!)', 1);
   yield* code().code('def greet() -> None:\n    print("Hello world!)', 1);
+  yield* code().selection(code().findFirstRange('None'), 0.6);
+  yield* code().selection(DEFAULT, 0.6);
+  yield* code().code('def greet() -> str:\n    return "Hello world!"', 1);
+  yield* code().selection(code().findFirstRange('str'), 0.6);
+  yield* code().selection(code().findFirstRange('return'), 0.6);
+  yield* code().selection(code().findFirstRange('"Hello world!"'), 0.6);
+  yield* code().selection(DEFAULT, 0.6);
 
-  // yield* all(
-  //   code().code(`def greet() -> None:\n    print("Hello! My name is {name}. I'am {age} years old.`, 1),
-  //   code().fontSize(28, 1),
-  // );
+  yield* code().code('def greet(name: str, age: int) -> str:\n    return "Hello world!"', 1);
+  yield* code().selection(code().findFirstRange('name'), 0.6);
+  yield* code().selection(code().findFirstRange('str'), 0.6);
+  yield* code().selection(code().findFirstRange('age'), 0.6);
+  yield* code().selection(code().findFirstRange('int'), 0.6);
+  yield* code().selection(DEFAULT, 0.6);
+  yield* all(
+    code().code(`def greet(name: str, age: int) -> str:\n    return f"Hello! My name is {name}. I'am {age} years old."`, 1),
+    // code().code(`def greet() -> None:\n    print("Hello! My name is {name}. I'am {age} years old.`, 1),
+    code().fontSize(28, 1),
+  );
+  yield* code().selection(word(1, 32, 4), 0.6);
+  yield* code().selection(word(1, 45, 3), 0.6);
+  yield* code().selection(DEFAULT, 0.6);
     // code().code('def greet():\n    print("Hello world!)', 1),
+  //
+  yield* all(
+    fx().children()[0].text("greet(name: str, age: int)"),
+    disappear(code()),
+    fx().children()[1].width(250, 1, easeInOutCubic),
+    fx().children()[1].height(100, 1, easeInOutCubic),
+  );
 
+  yield* fx().children()[0].text("Function", 1);
+
+  yield* disappear(fx());
 
   yield* waitFor(1);
 });
